b44f4ff842a9955c3507d207115be1e1cb2fcbcf
Swaraj <swarajrajpure@gmail.com>
2021-06-11 18:22:19 +0530

    adds whitenoise gunicorn, updates settings for prod

--- backend/urls.py
+++ backend/urls.py
@@ -19,5 +19,7 @@ from .views import summarize
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', summarize)
+    path('', summarize),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
